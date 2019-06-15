94e41ac56a673b8e2afd034da1e7b9960fc9c4f9
shivam gupta <shivamg8797@gmail.com>
2019-06-15 15:02:48 +0530

    scroll to top

--- public/js/main.js
+++ public/js/main.js
@@ -50,4 +50,29 @@ $(document).ready(function() {
       }
     ]
   });
+  // declare variable
+  var scrollTop = $('.scrollTop');
+
+  $(window).scroll(function() {
+    // declare variable
+    var topPos = $(this).scrollTop();
+
+    // if user scrolls down - show scroll to top button
+    if (topPos > 100) {
+      $(scrollTop).css('opacity', '1');
+    } else {
+      $(scrollTop).css('opacity', '0');
+    }
+  }); // scroll END
+
+  //Click event to scroll to top
+  $(scrollTop).click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: 0
+      },
+      800
+    );
+    return false;
+  });
 });
